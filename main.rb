3ea9c822887dc7dd91094e5f12ce91c953d58fd8
Jamie Ly <jamie.ly@gmail.com>
2011-12-24 00:08:47 -0500

    Got page collection working for the first example.

--- main.rb
+++ main.rb
@@ -1,26 +1,7 @@
 require 'mechanize'
 
-url = "http://www.spin.com/gallery/spins-20-best-photos-2010+?page=1#main"
-
-
-class UrlChainer
-  def initialize(base_url)
-    @prev_url = base_url
-    matches = /page=(?<page_number>\d+)/.match(@prev_url)
-    @base_page_number = matches[:page_number].to_i
-    @prev_page_number = @base_page_number
-  end
-  def next
-    @next_page_number = @prev_page_number + 1
-    @next_url = @prev_url.sub("page=#{@prev_page_number}", "page=#{@next_page_number}")
-    @prev_page_number = @next_page_number
-    @prev_url = @next_url
-
-    @next_url
-  end
-end
-
-def PageChain
+# Stores a chain of pages
+class PageChain
   def initialize(base_url)
     @pages = []
     @url = base_url
@@ -28,15 +9,36 @@ def PageChain
     @agent = Mechanize.new
   end
 
-  def retrieve
-    chainer = UrlChainer.new @url
-    @pages = [get_page(@url)]
+  def filter_links(links)
+    links.select {|link| /next/ =~ link.node.to_xml}
+  end
 
+  def retrieve
+    chainer = UrlChainer.new @agent, @url
+
+    # get the base page
+    page = get_page(@url)
+    @pages = [page]
+
+    prevent_cycles = {}
     begin
-      next_url = chainer.next
-      puts "Retrieving url #{next_url}"
-      page = get_page agent, next_url
-      @pages << page unless page.nil?
+      possible_links = filter_links page.links
+      first_link = possible_links.first
+
+      page = nil # always reset to nil
+      unless first_link.nil?
+        key = first_link.uri.to_s
+
+        # don't retrieve the page again if the URI is the same
+        unless prevent_cycles.key? key
+          prevent_cycles[key] = TRUE
+
+          puts "Retrieving #{key}"
+
+          page = first_link.click
+          @pages << page unless page.nil?
+        end
+      end
     end while not page.nil?
 
     @pages
@@ -49,14 +51,18 @@ def PageChain
     begin
       page = @agent.get(new_url)
     rescue Mechanize::ResponseCodeError
+      # Such as 404
     end
 
     page
   end
-
 end
 
+url = "http://www.spin.com/gallery/spins-20-best-photos-2010+?page=1#main"
+#url = "http://www.people.com/people/package/gallery/0,,20543425_20547825,00.html#21061464"
 
+page_chain = PageChain.new url
+pages = page_chain.retrieve
 
 puts "Number of pages: #{pages.length}"
 puts "Page Titles: %s" % [pages.collect(&:title)]
